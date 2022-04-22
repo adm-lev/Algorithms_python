aec58c718a4c9d816e1a748d56e002f67904517e
Eugene <rheingoldgalaxy@gmail.com>
2022-04-22 18:14:08 +0300

    сохранены 9 задач со ссылками  на графики

--- lesson_2_4.py
+++ lesson_2_4.py
@@ -1,5 +1,5 @@
 """
-
+https://drive.google.com/file/d/1lbDmorKEVNW1aGfTm4FM3Sv3kPy9e2-2/view?usp=sharing
  Найти сумму n элементов следующего ряда чисел: 1, -0.5, 0.25, -0.125,…
  Количество элементов (n) вводится с клавиатуры
 
