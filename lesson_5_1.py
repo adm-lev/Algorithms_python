e8048836b7e051e3ecb513d3f2d50491e1803fb8
Eugene <rheingoldgalaxy@gmail.com>
2022-05-18 18:05:50 +0300

    Черновик к 5 уроку

---
+++ lesson_5_1.py
@@ -0,0 +1,57 @@
+"""
+
+Пользователь вводит данные о количестве предприятий, их наименования и прибыль
+за 4 квартал (т.е. 4 числа) для каждого предприятия. Программа должна определить
+среднюю прибыль (за год для всех предприятий) и отдельно вывести наименования
+предприятий, чья прибыль выше среднего и ниже среднего.
+
+"""
+
+from collections import namedtuple
+
+Company = namedtuple('Company', 'name quart1 quart2 quart3 quart4 is_leader yearly')
+
+number = int(input('Здравствуйте! Введите, пожалуйста, общее число представленных компаний: '))
+all_companies = []
+income = 0
+avg_income = 0
+profitable = []
+unprofitable = []
+
+
+def print_res(org_list):
+    for _ in org_list:
+        print(_, end=', ')
+
+
+for i in range(number):
+    name = input(f'Введите имя {i + 1} компании: ')
+    quart1 = int(input(f'Введите прибыль за 1 квартал: '))
+    quart2 = int(input(f'Введите прибыль за 2 квартал: '))
+    quart3 = int(input(f'Введите прибыль за 3 квартал: '))
+    quart4 = int(input(f'Введите прибыль за 4 квартал: '))
+
+    all_companies.append(Company(name, quart1, quart2, quart3, quart4, True,
+                                 quart1 + quart2 + quart3 + quart4))
+    income += all_companies[i].yearly
+
+avg_income = income / number
+
+for i in all_companies:
+    if i.yearly >= avg_income:
+        profitable.append(i.name)
+    else:
+        unprofitable.append(i.name)
+
+print(f'Общая прибыль равна: {income}\nПрибыльные организации: ')
+print_res(profitable)
+print('\nУбыточные организации: ')
+print_res(unprofitable)
+
+
+
+
+
+
+
+
